a60043db91a02337c9e393ac3a00624ee5d10bb1
mpereira <prestataire.mario.pereira@ca-titres.fr>
2017-01-31 18:03:32 +0100

    my implementation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
